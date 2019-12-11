3c8f9ab00c0c898590a762970282c13f914f8dc8
bahaakhalf <bahaakhalf8@gmail.com>
2019-12-11 15:55:21 +0200

    commit

---
+++ src/eg/edu/alexu/csd/oop/game/Clown_world.java
@@ -0,0 +1,60 @@
+package eg.edu.alexu.csd.oop.game;
+import java.util.List;
+
+public class Clown_world implements World {
+
+	@Override
+	public List<GameObject> getConstantObjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GameObject> getMovableObjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<GameObject> getControlableObjects() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getWidth() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getHeight() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public boolean refresh() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getStatus() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getControlSpeed() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
