9317e3e143fecb766dd2f3f630abe7f67b415aa6
Islam Mohamed <islammohamed323@gmail.com>
2019-12-12 14:11:56 +0000

    without any organization

--- src/eg/edu/alexu/csd/oop/game/Clown_obj.java
+++ src/eg/edu/alexu/csd/oop/game/Clown_obj.java
@@ -1,54 +1,71 @@
 package eg.edu.alexu.csd.oop.game;
 import java.awt.image.BufferedImage;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 
 public class Clown_obj implements GameObject {
-
-	@Override
-	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+	private static final int MAX_MSTATE = 1;
+	// an array of sprite images that are drawn sequentially
+	private BufferedImage[] spriteImages = new BufferedImage[MAX_MSTATE];
+	private int x;
+	private int y;
+	private boolean visible;
+	
+	public Clown_obj(int posX, int posY, String path){
+		this(posX, posY, path, 0);
+	}
+	
+	public Clown_obj(int posX, int posY, String path, int type){
+		this.x = posX;
+		this.y = posY;
+		this.visible = true;
+		// create a bunch of buffered images and place into an array, to be displayed sequentially
+		try {
+			spriteImages[0] = ImageIO.read(getClass().getResourceAsStream(path));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	@Override
-	public void setX(int x) {
-		// TODO Auto-generated method stub
-		
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public void setX(int mX) {
+		this.x = mX;
 	}
 
 	@Override
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 	@Override
-	public void setY(int y) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	@Override
-	public boolean isVisible() {
-		// TODO Auto-generated method stub
-		return false;
+	public void setY(int mY) {
+		this.y = mY;
 	}
 
 	@Override
 	public BufferedImage[] getSpriteImages() {
-		// TODO Auto-generated method stub
-		return null;
+		return spriteImages;
 	}
+
+	@Override
+	public int getWidth(){
+		return spriteImages[0].getWidth();
+	}
+
+	@Override
+	public int getHeight() {
+		return spriteImages[0].getHeight();
+	}
+
+	@Override
+	public boolean isVisible() {
+		return visible;
+	}
+	
 
 }
